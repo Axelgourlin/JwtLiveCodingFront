1188330d608b686e929e05c092cf802c85e54445
Axel Gourlin <gourlin.axel@gmail.com>
2021-10-28 17:05:23 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import Login from "./components/Login";
-import Signin from "./components/Signin";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <Login />
-      <Signin />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
